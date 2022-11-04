a2d15b28d66d84abc076a94a36412d4797137b78
Timmy Omolana <timmieomolanakiel@gmail.com>
2022-11-04 15:35:29 +0000

    Add student matriculation details

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -42,31 +42,36 @@ Partial Class Form1
         Me.lblImag = New System.Windows.Forms.Label()
         Me.Xlabel = New System.Windows.Forms.Label()
         Me.lblX = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'lblForm
         '
-        Me.lblForm.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.lblForm.Location = New System.Drawing.Point(12, 5)
+        Me.lblForm.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblForm.Location = New System.Drawing.Point(196, 76)
         Me.lblForm.Name = "lblForm"
         Me.lblForm.Size = New System.Drawing.Size(466, 51)
         Me.lblForm.TabIndex = 0
         Me.lblForm.Text = "Quadratic Equation Solver"
+        Me.lblForm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Label2
         '
         Me.Label2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label2.Location = New System.Drawing.Point(21, 59)
+        Me.Label2.Location = New System.Drawing.Point(21, 121)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(415, 34)
+        Me.Label2.Size = New System.Drawing.Size(665, 34)
         Me.Label2.TabIndex = 2
-        Me.Label2.Text = "You are required to input the values of a, b, and c below."
+        Me.Label2.Text = "To solve for ax^2 +  bx + c = 0, you are required to input the values of a, b, an" &
+    "d c below."
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label1.Location = New System.Drawing.Point(25, 107)
+        Me.Label1.Location = New System.Drawing.Point(25, 162)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(121, 15)
         Me.Label1.TabIndex = 3
@@ -76,7 +81,7 @@ Partial Class Form1
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label3.Location = New System.Drawing.Point(25, 136)
+        Me.Label3.Location = New System.Drawing.Point(25, 191)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(121, 15)
         Me.Label3.TabIndex = 4
@@ -86,7 +91,7 @@ Partial Class Form1
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label4.Location = New System.Drawing.Point(25, 169)
+        Me.Label4.Location = New System.Drawing.Point(25, 224)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(120, 15)
         Me.Label4.TabIndex = 5
@@ -96,7 +101,7 @@ Partial Class Form1
         '
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label5.Location = New System.Drawing.Point(12, 250)
+        Me.Label5.Location = New System.Drawing.Point(12, 276)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(115, 37)
         Me.Label5.TabIndex = 6
@@ -104,21 +109,21 @@ Partial Class Form1
         '
         'txtA
         '
-        Me.txtA.Location = New System.Drawing.Point(149, 105)
+        Me.txtA.Location = New System.Drawing.Point(149, 160)
         Me.txtA.Name = "txtA"
         Me.txtA.Size = New System.Drawing.Size(287, 23)
         Me.txtA.TabIndex = 7
         '
         'txtB
         '
-        Me.txtB.Location = New System.Drawing.Point(149, 135)
+        Me.txtB.Location = New System.Drawing.Point(149, 190)
         Me.txtB.Name = "txtB"
         Me.txtB.Size = New System.Drawing.Size(287, 23)
         Me.txtB.TabIndex = 8
         '
         'txtC
         '
-        Me.txtC.Location = New System.Drawing.Point(149, 166)
+        Me.txtC.Location = New System.Drawing.Point(149, 221)
         Me.txtC.Name = "txtC"
         Me.txtC.Size = New System.Drawing.Size(287, 23)
         Me.txtC.TabIndex = 9
@@ -126,7 +131,7 @@ Partial Class Form1
         'calcButton
         '
         Me.calcButton.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.calcButton.Location = New System.Drawing.Point(25, 222)
+        Me.calcButton.Location = New System.Drawing.Point(296, 254)
         Me.calcButton.Name = "calcButton"
         Me.calcButton.Size = New System.Drawing.Size(140, 25)
         Me.calcButton.TabIndex = 10
@@ -136,7 +141,7 @@ Partial Class Form1
         'x1Label
         '
         Me.x1Label.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.x1Label.Location = New System.Drawing.Point(21, 329)
+        Me.x1Label.Location = New System.Drawing.Point(21, 355)
         Me.x1Label.Name = "x1Label"
         Me.x1Label.Size = New System.Drawing.Size(106, 15)
         Me.x1Label.TabIndex = 11
@@ -146,7 +151,7 @@ Partial Class Form1
         '
         Me.lblX1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.lblX1.Font = New System.Drawing.Font("Segoe UI", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.lblX1.Location = New System.Drawing.Point(126, 326)
+        Me.lblX1.Location = New System.Drawing.Point(126, 352)
         Me.lblX1.Name = "lblX1"
         Me.lblX1.Size = New System.Drawing.Size(152, 23)
         Me.lblX1.TabIndex = 12
@@ -154,7 +159,7 @@ Partial Class Form1
         'x2Label
         '
         Me.x2Label.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.x2Label.Location = New System.Drawing.Point(21, 364)
+        Me.x2Label.Location = New System.Drawing.Point(21, 390)
         Me.x2Label.Name = "x2Label"
         Me.x2Label.Size = New System.Drawing.Size(25, 15)
         Me.x2Label.TabIndex = 14
@@ -164,7 +169,7 @@ Partial Class Form1
         '
         Me.lblX2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.lblX2.Font = New System.Drawing.Font("Segoe UI", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.lblX2.Location = New System.Drawing.Point(126, 356)
+        Me.lblX2.Location = New System.Drawing.Point(126, 382)
         Me.lblX2.Name = "lblX2"
         Me.lblX2.Size = New System.Drawing.Size(150, 23)
         Me.lblX2.TabIndex = 15
@@ -173,7 +178,7 @@ Partial Class Form1
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label6.Location = New System.Drawing.Point(21, 397)
+        Me.Label6.Location = New System.Drawing.Point(21, 423)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(60, 15)
         Me.Label6.TabIndex = 16
@@ -182,7 +187,7 @@ Partial Class Form1
         'lblReal
         '
         Me.lblReal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.lblReal.Location = New System.Drawing.Point(126, 387)
+        Me.lblReal.Location = New System.Drawing.Point(126, 413)
         Me.lblReal.Name = "lblReal"
         Me.lblReal.Size = New System.Drawing.Size(150, 23)
         Me.lblReal.TabIndex = 17
@@ -191,7 +196,7 @@ Partial Class Form1
         '
         Me.Label8.AutoSize = True
         Me.Label8.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label8.Location = New System.Drawing.Point(20, 425)
+        Me.Label8.Location = New System.Drawing.Point(20, 451)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(91, 15)
         Me.Label8.TabIndex = 18
@@ -200,7 +205,7 @@ Partial Class Form1
         'lblImag
         '
         Me.lblImag.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.lblImag.Location = New System.Drawing.Point(126, 418)
+        Me.lblImag.Location = New System.Drawing.Point(126, 444)
         Me.lblImag.Name = "lblImag"
         Me.lblImag.Size = New System.Drawing.Size(150, 24)
         Me.lblImag.TabIndex = 19
@@ -208,7 +213,7 @@ Partial Class Form1
         'Xlabel
         '
         Me.Xlabel.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Xlabel.Location = New System.Drawing.Point(20, 298)
+        Me.Xlabel.Location = New System.Drawing.Point(20, 324)
         Me.Xlabel.Name = "Xlabel"
         Me.Xlabel.Size = New System.Drawing.Size(100, 23)
         Me.Xlabel.TabIndex = 20
@@ -217,16 +222,49 @@ Partial Class Form1
         'lblX
         '
         Me.lblX.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.lblX.Location = New System.Drawing.Point(126, 298)
+        Me.lblX.Location = New System.Drawing.Point(126, 324)
         Me.lblX.Name = "lblX"
         Me.lblX.Size = New System.Drawing.Size(150, 23)
         Me.lblX.TabIndex = 21
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label7.Location = New System.Drawing.Point(264, 9)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(373, 28)
+        Me.Label7.TabIndex = 22
+        Me.Label7.Text = "NAME: OMOLANA TIMILEHIN EZEKIEL"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label9.Location = New System.Drawing.Point(264, 37)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(231, 21)
+        Me.Label9.TabIndex = 23
+        Me.Label9.Text = "MATRIC NUMBER: 210806079"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label10.Location = New System.Drawing.Point(264, 58)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(347, 21)
+        Me.Label10.TabIndex = 24
+        Me.Label10.Text = "DEPARTMENT: MATHEMATICS (INDUSTRIAL)"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(838, 473)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.lblX)
         Me.Controls.Add(Me.Xlabel)
         Me.Controls.Add(Me.lblImag)
@@ -275,4 +313,7 @@ Partial Class Form1
     Friend WithEvents lblImag As Label
     Friend WithEvents Xlabel As Label
     Friend WithEvents lblX As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
 End Class
